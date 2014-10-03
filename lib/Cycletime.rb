590e9e9938a9dbf8245f7ed7e2fcaf2bec380162
iainjmitchell <iainjmitchell@gmail.com>
2014-10-03 17:02:55 +0100

    refactoring

--- lib/Cycletime.rb
+++ lib/Cycletime.rb
@@ -4,17 +4,19 @@ require_relative './BoardSixWeekCycleTime'
 require_relative './MongoRepository'
 require_relative './BoardCycleTimesFactory'
 
-file = File.read("#{File.dirname(__FILE__)}/config.json")
-cycle_time_calculations = JSON.parse(file)
-repository = MongoRepository.new(ENV['MONGO_CONN'], 'production', 'cycle_time')
-board_cycle_times_factory = BoardCycleTimesFactory.new(repository)
+unless Time.now.saturday? || Time.now.sunday?
+	file = File.read("#{File.dirname(__FILE__)}/config.json")
+	cycle_time_calculations = JSON.parse(file)
+	repository = MongoRepository.new(ENV['MONGO_CONN'], 'production', 'cycle_time')
+	board_cycle_times_factory = BoardCycleTimesFactory.new(repository)
 
-trello_cycle_time = AgileTrello::TrelloCycleTime.new(
-	public_key: 'e185a8128064891a8961802a3d86b08e',
-	access_token: ENV['TRELLO_TOKEN']
-)
+	trello_cycle_time = AgileTrello::TrelloCycleTime.new(
+		public_key: 'e185a8128064891a8961802a3d86b08e',
+		access_token: ENV['TRELLO_TOKEN']
+	)
 
-cycle_time_calculations.each do |cycle_time_calculation|
-	board_cycle_times = board_cycle_times_factory.create(cycle_time_calculation["name"])
-	BoardSixWeekCycleTime.new(trello_cycle_time, board_cycle_times, cycle_time_calculation).calculate
+	cycle_time_calculations.each do |cycle_time_calculation|
+		board_cycle_times = board_cycle_times_factory.create(cycle_time_calculation["name"])
+		BoardSixWeekCycleTime.new(trello_cycle_time, board_cycle_times, cycle_time_calculation).calculate
+	end
 end
