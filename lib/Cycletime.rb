2187db3c776ab23b7e7d45db59e14e5aea2b1b63
iainjmitchell <iainjmitchell@gmail.com>
2014-12-09 13:50:55 +0000

    getting rid of silly issue that averaged cycle time across ALL boards!

--- lib/Cycletime.rb
+++ lib/Cycletime.rb
@@ -10,12 +10,11 @@ unless Time.now.saturday? || Time.now.sunday?
 	repository = MongoRepository.new(ENV['MONGO_CONN'], 'production', 'cycle_time')
 	board_cycle_times_factory = BoardCycleTimesFactory.new(repository)
 
-	trello_cycle_time = AgileTrello::TrelloCycleTime.new(
-		public_key: 'e185a8128064891a8961802a3d86b08e',
-		access_token: ENV['TRELLO_TOKEN']
-	)
-
 	cycle_time_calculations.each do |cycle_time_calculation|
+		trello_cycle_time = AgileTrello::TrelloCycleTime.new(
+			public_key: 'e185a8128064891a8961802a3d86b08e',
+			access_token: ENV['TRELLO_TOKEN']
+		)
 		board_cycle_times = board_cycle_times_factory.create(cycle_time_calculation["name"])
 		BoardSixWeekCycleTime.new(trello_cycle_time, board_cycle_times, cycle_time_calculation).calculate
 	end
